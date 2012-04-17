ca428499702b7d2bbdfe0a830516cb33a12e5334
Kurt <git@it-marx.de>
2012-04-17 16:41:05 +0200

    init

---
+++ src/CheckPoint.java
@@ -0,0 +1,16 @@
+/**
+ * @author Kurt
+ *
+ */
+public class CheckPoint {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("First commit, yeaah");
+
+	}
+
+}
