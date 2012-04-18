8d22abb8c39526f40afeb12d8929dce86ebed259
Stefan Marx <Stefan Marx@Dell-PC-Buero>
2012-04-19 00:48:56 +0200

    new mainwindow class

---
+++ src/MainWindow.java
@@ -0,0 +1,77 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.jface.databinding.swt.SWTObservables;
+import org.eclipse.core.databinding.observable.Realm;
+import org.eclipse.swt.widgets.ToolItem;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+
+public class MainWindow {
+	protected Shell shell;
+	private ToolBar toolBar;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Display display = Display.getDefault();
+		Realm.runWithDefault(SWTObservables.getRealm(display), new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.open();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(477, 292);
+		shell.setText("SWT Application");
+		
+		toolBar = new ToolBar(shell, SWT.FLAT | SWT.RIGHT);
+		toolBar.setBounds(0, 0, 434, 23);
+		
+		ToolItem tltmExit = new ToolItem(toolBar, SWT.NONE);
+		tltmExit.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.exit(0);
+			}
+		});
+		tltmExit.setText("EXIT");
+		initDataBindings();
+
+	}
+	protected DataBindingContext initDataBindings() {
+		DataBindingContext bindingContext = new DataBindingContext();
+		//
+		return bindingContext;
+	}
+}
